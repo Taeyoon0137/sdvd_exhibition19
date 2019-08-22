d5122b379a0fbd0922f9d696fc2ec3c1c3c4bf1f
Taeyoon <taeyoon0137@gmail.com>
2019-08-23 01:52:32 +0900

    add navigation

--- assets/js/header_navbutton.js
+++ assets/js/header_navbutton.js
@@ -1,21 +1,17 @@
-
-window.addEventListener('scroll', function() {
-var el = document.querySelector('.header_background');
-
-if(window.scrollY >= 300px) el.classList.add('background');
-else el.classList.remove('background');
-});
-
 window.onload = ()=> {
   //header color
 
 
   //nav button
-  document.querySelector(".nav-button-border").addEventListener("click", ()=> {
-  document.querySelector("#header").classList.toggle("nav--active");
-  document.querySelector("#navigation").classList.toggle("nav--active");
-  document.querySelector("#wrapper").classList.toggle("nav--active");
-  })
-
+  document.querySelector(".btn_nav").addEventListener("click", ()=> {
+  document.querySelector("#nav_toggle").classList.toggle("nav-active");
+  document.querySelector(".nav").classList.toggle("nav-active");
   })
 }
+
+//window.addEventListener('scroll', function() {
+//var el = document.querySelector('.header_background');
+
+//if(window.scrollY >= 300px) el.classList.add('background');
+//else el.classList.remove('background');
+//});
