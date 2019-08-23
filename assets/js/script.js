2a3345f6d29eae642f4360e5ddce7d5457c7c90d
Taeyoon <taeyoon0137@gmail.com>
2019-08-23 12:14:20 +0900

    I hate safari

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var w_width = $(window).width();
   var h_height = $(window).height();
+  var nav_height = h_height - 201;
+
 
   $("#landing").height(h_height);
+  $("#nav_height").height(nav_height);
 })
