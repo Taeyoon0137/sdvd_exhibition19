e6f2c9dd085b968d0aa4fdafa857690b9163a256
Taeyoon <taeyoon0137@gmail.com>
2019-08-25 21:05:21 +0900

    add map

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
 
   $(".landing").height(h_height);
 })
+
+var container = document.getElementById('map');
+var options = {
+	center: new kakao.maps.LatLng(33.450701, 126.570667),
+	level: 3
+};
+
+var map = new kakao.maps.Map(container, options);
