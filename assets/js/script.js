439c2b518f5cd2d99d4e04309707b9a6f9477957
Taeyoon <taeyoon0137@gmail.com>
2019-08-25 11:51:19 +0900

    0.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
   var w_width = $(window).width();
   var h_height = $(window).height();
 
-  $("#landing").height(h_height);
-  $("#nav_height").height(nav_height);
+  $(".landing").height(h_height);
 })
